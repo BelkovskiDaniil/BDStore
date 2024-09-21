3dca8b816f08b6d024298eef7403a7bf12dad702
BelkovskiDaniil <74908833+BelkovskiDaniil@users.noreply.github.com>
2024-09-21 20:49:31 +0500

    Initial commit

---
+++ src/main/java/com/example/BDStore/BdStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.BDStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdStoreApplication.class, args);
+	}
+
+}
